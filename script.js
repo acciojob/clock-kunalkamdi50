7e5e5295c402f8548b4075f6729cde5b54254a16
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-04-28 00:01:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 function getTime(){
 	const time = new Date();
-	let dayString ='${time.getMonth()+1}/${time.getDate()}/${time.getFullYear()}';
-let hr = time.getHour();
+	let dayString =`${time.getMonth()+1}/${time.getDate()}/${time.getFullYear()}`;
+let hr = time.getHours();
 	let timeString ="";
 	if(hr>= 12){
 		if(hr == 12){

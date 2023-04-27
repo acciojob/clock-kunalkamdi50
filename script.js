63f8835e2d250339203133fd4cf4b9a06b877abf
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-04-27 23:43:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+function getTime(){
+	const time = new Date();
+	let dayString ='${time.getMonth()+1}/${time.getDate()}/${time.getFullYear()}';
+let hr = time.getHour();
+	let timeString ="";
+	if(hr>= 12){
+		if(hr == 12){
+			timeString =`${time.getHour()}:${time.getMinutes()}:${time.getSecond()} PM`
+			}
+		else{
+			hr -=12 ;
+			timeString=`${hr}:${time.getMinutes()}:${time.getSeconds()} PM`
+		}
+	}
+	else{
+		//:Am
+		//: 12 hr clock modification needed
+		timeString = `${time.getHours()}:${time.getMinutes()}:${time.getSecond()} AM`
+	}
+	return dayString+", "+timeString;
+}
+const p = document.getElementById("timer");
+
+function timer(){
+	p.innerText = getTime();
+}
+
+setInterval(timer, 1000);
